8c029891775f33a6c9939fd38acbc42e9eb0bb36
diegoblazquezr <diegoblazquezr@gmail.com>
2024-07-02 18:02:46 +0200

    small fix routes search

--- routes/web.routes.js
+++ routes/web.routes.js
@@ -1,4 +1,5 @@
 const webController = require('../controllers/web.controllers');
+const jobService = require('../services/jobs.services');
 const router = require('express').Router();
 
 // GET http://localhost::3000/
@@ -10,5 +11,30 @@ router.get("/favorites", webController.getFavorites);
 router.get("/profile", webController.getProfile);
 router.get("/users", webController.getUsers);
 router.get("/dashboard", webController.getDashboard);
+
+// Ruta para manejar la búsqueda por keyword
+router.post('/search', async (req, res) => {
+    try {
+        const keyword = req.body.keyword || null;
+        const updatedJobs = await jobService.readJobs(keyword);
+        console.log(updatedJobs)
+        res.status(200).render("home.pug", { jobs: updatedJobs });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Ruta para manejar la búsqueda por skills
+router.post('/searchbyskill', async (req, res) => {
+    try {
+        const skill = req.body.skill || null;
+        const updatedJobs = await jobService.readJobsBySkill(skill);
+        console.log(updatedJobs)
+        res.status(200).render("home.pug", { jobs: updatedJobs });
+    } catch (error) {
+        console.error('Error searching jobs by skill:', error);
+        res.status(500).json({ error: error.message });
+    }
+});
 
 module.exports = router;
