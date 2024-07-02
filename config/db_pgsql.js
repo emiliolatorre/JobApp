f2f864fecdad424c4a2293a6190cd745d2484e8e
diegoblazquezr <diegoblazquezr@gmail.com>
2024-07-02 19:09:52 +0200

    render deploy config changes

--- config/db_pgsql.js
+++ config/db_pgsql.js
@@ -3,38 +3,27 @@ require('dotenv').config()
 
 // Datos de conexión
 
+// const pool = new Pool({
+//     host: 'localhost',
+//     user: 'postgres',
+//     port: '5432',
+//     database: 'jobsappsql',
+//     password: '123456'
+// });
+
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    port: '5432',
-    database: 'jobsappsql',
-    password: '123456'
+    host: process.env.RENDER_HOST,
+    user: process.env.RENDER_USER,
+    password: process.env.RENDER_PASSWORD,
+    database: process.env.RENDER_DATABASE,
+    port: process.env.RENDER_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
-// const pool = new Pool({
-//     host: HOST,
-//     user: USER,
-//     password: PASSWORD,
-//     database: DATABASE,
-//     port: PORT,
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
-// });
-
-// const pool = new Pool({
-//     host: 'dpg-cpoqo6iju9rs738ufcc0-a.frankfurt-postgres.render.com',
-//     user: 'userrenderpostgresql',
-//     password: 'YbK3LLwigCfRy70zIwddDSSmm29TKgY8',
-//     database: 'BBDD-Authors-Entries',
-//     port: '5432',
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
-// });
-
-// pool.connect()
-//     .then(() => console.log('Conexión exitosa'))
-//     .catch(err => console.log('Error de conexión', err.stack))
+pool.connect()
+    .then(() => console.log('Connection to pgsql established'))
+    .catch(err => console.log('Pgsql error connection', err.stack))
 
 module.exports = pool;
